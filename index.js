1c1cf03bd1f3cbb2c221f64a49f44ad06700593a
青章浚 <39018081+JupiterJun@users.noreply.github.com>
2021-09-26 19:52:08 +0800

    二改前

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const pyfl = require('pyfl').default;
+const pinyin = require('pinyin')
 const inquirer = require('inquirer');
 const fs = require('fs');
 const colors = require('colors');
@@ -6,21 +7,26 @@ const exec = require('child_process').exec;
 const iconv = require('iconv-lite');
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
+
 const adapter = new FileSync('db.json');
 const db = low(adapter);
+const print = console.log;
 
+// 程序主入口
 (async function () {
+    // 欢迎和初始化数据库
     print('Hello there, enter "exit" to exit.');
-
     generateDb();
 
+    // 循环体
     while (true) {
         let input = await inquirer.prompt([{
             type: 'input',
             message: 'correct-name',
             name: 'name',
             prefix: '',
-            suffix: ' >'
+            suffix: ' >',
+            filter: a => a.toLocaleUpperCase()
         }])
         if (input.name == 'exit') {
             db.set('names', []).write();
@@ -31,8 +37,7 @@ const db = low(adapter);
             generateDb();
             continue;
         };
-        let queryPy = input.name.toLocaleUpperCase();
-        let resultList = db.get('names').filter({ Pinyin: queryPy }).map('Name').value();
+        let resultList = db.get('names').filter({ Pinyin: input.name }).map('Name').value();
 
         switch (resultList.length) {
             case 0:
@@ -57,10 +62,7 @@ const db = low(adapter);
 
 })();
 
-function print(text) {
-    console.log(text);
-};
-
+// 将“\n”换成真正的换行符
 function byLine(string) {
     let i = 0;
     let arr = [];
@@ -74,22 +76,36 @@ function byLine(string) {
     return arr;
 }
 
+// 生成首字母数据库
 function generateDb() {
     let namelist = []
     try {
         namelist = byLine(fs.readFileSync('namelist.txt', { encoding: 'utf8' }));
     } catch (error) {
-        fs.writeFileSync('namelist.txt','\n');
+        fs.writeFileSync('namelist.txt', '\n');
     }
     if (namelist.length != 0) {
         var namepy = []
-        for (i of namelist) namepy.push({ Name: i, Pinyin: pyfl(i) });
+        for (i of namelist) namepy.push({ Name: i, Pinyin: generatePy(i) });
         db.set('names', namepy).write();
         print('Generate database successed! Now you can use it!'.green)
     }
     else print('Please put some names in "namelist.txt". One line one name.\nThen enter "generate" to generate a database to use.'.red);
 }
 
+// 针对不同平台的复制函数
 function copy(text) {
     exec('clip').stdin.end(iconv.encode(text, 'gbk'));
+}
+
+// 生成一个拼音首字母
+function generatePy(i) {
+    let py = pyfl(i);
+    // let py = pinyin(i, { heteronym: true, style: pinyin.STYLE_FIRST_LETTER })
+    return py
+}
+
+// 查询一个拼音，返回匹配的列表
+function queryName(n) {
+    
 }
